2f109b8cc29e0f4b5b2e2a21a21bc933c10bd3c8
William <wanderson@relatient.net>
2020-02-03 14:48:02 -0600

    reinitailized project with SCSS

--- angular-electron/src/app/app.module.ts
+++ angular-electron/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
